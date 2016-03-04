82cde29f6f75de841d7f7e4fa15ab3a16da5fd02
SJQ <shuangjiaqi@gmail.com>
2016-03-04 10:43:15 +0800

    add CollectionUtil class

--- src/main/java/com/sjq/smart4k/utils/CollectionUtil.java
+++ src/main/java/com/sjq/smart4k/utils/CollectionUtil.java
@@ -1,7 +1,55 @@
 package com.sjq.smart4k.utils;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.collections4.trie.AbstractBitwiseTrie;
+
+import java.util.Collection;
+import java.util.Map;
+
 /**
+ * A Collection Util Class
+ *
  * Created by sjq on 16/3/3.
  */
-public class CollectionUtil {
+public final class CollectionUtil {
+    /**
+     * Null-safe check if the specified collection is empty.
+     *
+     * @param collection
+     * @return
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return CollectionUtils.isEmpty(collection);
+    }
+
+    /**
+     *  Null-safe check if the specified collection is not empty.
+     *
+     * @param collection
+     * @return
+     */
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     *  Null-safe check if the specified map is empty.
+     *
+     * @param map
+     * @return
+     */
+    public static boolean isEmpty(Map<?,?> map) {
+        return MapUtils.isEmpty(map);
+    }
+
+    /**
+     *  Null-safe check if the specified map is not empty.
+     *
+     * @param map
+     * @return
+     */
+    public static boolean isNotEmpty(Map<?,?> map) {
+        return !isEmpty(map);
+    }
 }
