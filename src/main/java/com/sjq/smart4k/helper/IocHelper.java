b20baa046e2fbc6c1145c8d50e1cf1ccba938962
SJQ <shuangjiaqi@gmail.com>
2016-03-04 11:16:55 +0800

    add IocHelper and ArrayUtil

---
+++ src/main/java/com/sjq/smart4k/helper/IocHelper.java
@@ -0,0 +1,39 @@
+package com.sjq.smart4k.helper;
+
+import com.sjq.smart4k.annotation.Inject;
+import com.sjq.smart4k.utils.ArrayUtil;
+import com.sjq.smart4k.utils.CollectionUtil;
+import com.sjq.smart4k.utils.ReflectionUtil;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * A Inversion of Control Class
+ * Created by user on 2016/3/4.
+ */
+public final class IocHelper {
+    static {
+        Map<Class<?>, Object> beanMap = BeanHelper.getBeanMap();
+        if (CollectionUtil.isNotEmpty(beanMap)) {
+            for (Map.Entry<Class<?>,Object> beanEntry : beanMap.entrySet()) {
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if (ArrayUtil.isNotEmpty(beanFields)) {
+                    for (Field beandField : beanFields) {
+                        if (beandField.isAnnotationPresent(Inject.class)) {
+                            Class<?> beanFieldClass = beandField.getType();
+                            Object beadnFiedInstance = beanMap.get(beanFieldClass);
+                            if (beadnFiedInstance != null) {
+                                // Initialize the value of Beanfield
+                                ReflectionUtil.setField(beanInstance, beandField, beadnFiedInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/sjq/smart4k/utils/ArrayUtil.java
@@ -0,0 +1,28 @@
+package com.sjq.smart4k.utils;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * Created by user on 2016/3/4.
+ */
+public final class ArrayUtil {
+    /**
+     * Checks if an array of Objects is empty or null
+     *
+     * @param array
+     * @return
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * Checks if an array Objects is not empty or null
+     *
+     * @param array
+     * @return
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !isEmpty(array);
+    }
+}
